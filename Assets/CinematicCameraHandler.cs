6f261520cc352f20ffe28474cf0f734fa35a8725
Flor Ramallo <fernantastic@gmail.com>
2024-01-16 17:21:19 -0800

    tutorial cameras, gray bg

--- Assets/CinematicCameraHandler.cs
+++ Assets/CinematicCameraHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -15,6 +16,9 @@ public class CinematicCameraHandler : MonoBehaviour
     
     Transform Head { get { 
         return God.wren.bird.head.transform;
+    }}
+    Vector3 EyePositionRight { get { 
+        return God.wren.bird.head.transform.TransformPoint(-.0562f, .0113f, .031f);
     }}
 
     public bool armed = false;
@@ -39,53 +43,106 @@ public class CinematicCameraHandler : MonoBehaviour
 
     }
     public CinematicCamera[] tutorialCameras;
+    public CinematicCameraDescriptor[] _cams;
     public int tutorialCameraIdx;
 
+    void Start()
+    {
+      _cams = GetComponentsInChildren<CinematicCameraDescriptor>();
+    }
 
     void LateUpdate()
     {
         if (!God.wren)
             return;
 
-        if (armed && tutorialCameras.Length > 0)
+        if (armed && _cams.Length > 0)
         {
-            var c = tutorialCameras[(int)Mathf.Clamp(tutorialCameraIdx, 0, tutorialCameras.Length-1)];
+            var c = _cams[(int)Mathf.Clamp(tutorialCameraIdx, 0, _cams.Length-1)];
             
-            GetCustomCameraPositions(c, out var cPos, out var tPos);
+            if (Application.isEditor)
+            {
+              foreach(var ct in GetComponentsInChildren<CinematicCameraDescriptor>())
+              {
+                if (ct.debugArmed)
+                {
+                  c = ct;
+                }
+              }
+            }
 
-            God.camera.transform.position = cPos;
-            God.camera.transform.LookAt(tPos, Vector3.up);
-            God.camera.fieldOfView = c.fov;
+            GetCustomCameraPositions(c, out var cPos, out var tPos);
 
         }
 
     }
 
-    void GetCustomCameraPositions(CinematicCamera cam, out Vector3 camPos, out Vector3 camTgt)
+    Transform GetTarget(CinematicCameraDescriptor.BodyTarget target)
     {
-      camPos = BirdTransform.position + cam.posOffset;
-      camTgt = BirdTransform.position + cam.lookOffset;
-
-
-      
-      if (cam.bodyTarget == CinematicCamera.BodyTarget.Head)
+      switch(target)
       {
-        camPos = Head.position;
+        case CinematicCameraDescriptor.BodyTarget.Head:
+        case CinematicCameraDescriptor.BodyTarget.Eye:
+          return Head;
       }
-      
-      if (cam.aimTarget == CinematicCamera.BodyTarget.Head)
-      {
-        camTgt = Head.position;
-      }
+      return BirdTransform;
+    }
+
+    void GetCustomCameraPositions(CinematicCameraDescriptor cam, out Vector3 pos, out Vector3 lookPos)
+    {
+
+      var bodyTarget = GetTarget(cam.bodyTarget);
+      var aimTarget = GetTarget(cam.aimTarget);
+
+      pos = cam.posOffset;
+      lookPos = aimTarget.position + cam.aimPosOffset;
+      if (cam.aimTarget == CinematicCameraDescriptor.BodyTarget.Eye)
+        lookPos = EyePositionRight + cam.aimPosOffset;
 
       if (cam.orbit)
       {
-        camPos = camPos + new Vector3(
-          Mathf.Sin(Mathf.Lerp(-Mathf.PI, Mathf.PI, cam.orbitAngle)) * cam.orbitRadius,
-          cam.orbitHeight,
-          Mathf.Cos(Mathf.Lerp(-Mathf.PI, Mathf.PI, cam.orbitAngle)) * cam.orbitRadius
+        var y = cam.orbitRadius * Mathf.Sin(cam.orbitHeight * Mathf.Deg2Rad);
+        var r = cam.orbitRadius * Mathf.Cos(cam.orbitHeight * Mathf.Deg2Rad);
+        var a = (cam.orbitAngle + .25f) % 1;
+        pos += new Vector3(
+          r * Mathf.Cos(Mathf.Lerp(-Mathf.PI, Mathf.PI, a)),
+          y,
+          r * Mathf.Sin(Mathf.Lerp(-Mathf.PI, Mathf.PI, a))
         );
       }
 
+      // Position
+      if (cam.parentToBird)
+      {
+        God.camera.transform.position = bodyTarget.TransformPoint(pos);
+        God.camera.transform.rotation = bodyTarget.rotation;
+        lookPos = aimTarget.TransformPoint(cam.aimPosOffset);
+      }
+      else
+      {
+        God.camera.transform.position = bodyTarget.position + pos;
+        God.camera.transform.rotation = bodyTarget.rotation;
+      }
+      // Rotation
+      if (cam.rotation == CinematicCameraDescriptor.Rotation.UseBodyRotation)
+        God.camera.transform.rotation = bodyTarget.rotation;
+      else if (cam.rotation == CinematicCameraDescriptor.Rotation.UseAimRotation)
+        God.camera.transform.rotation = aimTarget.rotation;
+      else if (cam.rotation == CinematicCameraDescriptor.Rotation.LookAt)
+        God.camera.transform.LookAt(lookPos, Vector3.up);
+      else
+        God.camera.transform.LookAt(lookPos, bodyTarget.up);
+      
+      God.camera.transform.Rotate(cam.rotationOffset, Space.Self);
+
+      // Etc
+      God.camera.fieldOfView = cam.fov;
+
+      cam.transform.position = God.camera.transform.position;
+      cam.transform.rotation = God.camera.transform.rotation;
+
+      Debug.DrawLine(cam.transform.position, lookPos);
+
     }
+
 }
