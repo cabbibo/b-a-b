871227c431157b44e60d37dafbc5bea12176ce88
Flor Ramallo <fernantastic@gmail.com>
2024-01-30 12:40:55 -0800

    tutorial card

---
+++ Assets/TutorialCardTrigger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using WrenUtils;
+
+public class TutorialCardTrigger : MonoBehaviour
+{
+    
+}
