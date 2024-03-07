fc8e7a2a3f3c89b3adbe7cc4459976f3f62c4eb0
cabbibo <isaaclandoncohen@gmail.com>
2024-03-07 08:25:28 -0800

    Fixing up place on depth map

--- Assets/Plugins/PlaceParticlesOnDepthMap/PlaceParticlesOnDepthMap.cs
+++ Assets/Plugins/PlaceParticlesOnDepthMap/PlaceParticlesOnDepthMap.cs
@@ -18,11 +18,7 @@ public class PlaceParticlesOnDepthMap : MonoBehaviour
 
     public Camera camera;
 
-
     public Material material;
-    public Material material2;
-    public Material material3;
-    public Material material4;
     public MaterialPropertyBlock mpb;
 
     public int structSize = 16;
@@ -37,13 +33,17 @@ public class PlaceParticlesOnDepthMap : MonoBehaviour
     RenderTextureDescriptor textureDescriptor;
 
     RenderTexture texture;
-    public int renderSize = 1024;
+    public Vector2 renderSize = new Vector2(1920, 1080);
+
+    public MeshRenderer debugDepthRenderer;
     void OnEnable()
     {
 
 
-        textureDescriptor = new RenderTextureDescriptor(renderSize, renderSize, RenderTextureFormat.Depth, 24);
+        textureDescriptor = new RenderTextureDescriptor((int)renderSize.x, (int)renderSize.y, RenderTextureFormat.Depth, 24);
         texture = RenderTexture.GetTemporary(textureDescriptor);
+
+        texture.filterMode = FilterMode.Trilinear;
 
 
         _VertBuffer = new ComputeBuffer(count, sizeof(float) * structSize);
@@ -60,8 +60,6 @@ public class PlaceParticlesOnDepthMap : MonoBehaviour
 
 
         if (_VertBuffer != null) { _VertBuffer.Release(); }
-        //_VertBuffer.Release();
-        //_VertBuffer.Dispose();
     }
 
 
@@ -73,23 +71,17 @@ public class PlaceParticlesOnDepthMap : MonoBehaviour
 
     public void GetNumGroups()
     {
-        // numGroups = ((int)count + ((int)numThreads - 1)) / (int)numThreads;
-        numGroups = ((int)count) / (int)numThreads;
+        numGroups = ((int)count + ((int)numThreads - 1)) / (int)numThreads;
+        //numGroups = ((int)count) / (int)numThreads;
     }
 
-    public TerrainData terrainData;
-
-    public Texture heightMap;
 
 
-
-    public MeshRenderer debugDepthRenderer;
 
     // Update is called once per frame
     void Update()
     {
 
-        // camera.CopyFrom(Camera.main);
         // Set out depth camera properties to be the same as the main camera
         camera.fieldOfView = Camera.main.fieldOfView;
         camera.nearClipPlane = Camera.main.nearClipPlane;
@@ -100,22 +92,11 @@ public class PlaceParticlesOnDepthMap : MonoBehaviour
         camera.transform.position = Camera.main.transform.position;
         camera.transform.rotation = Camera.main.transform.rotation;
 
-        // Render out the depth texture
-        //camera.targetTexture = depthTexture;
-
-        //camera.targetTexture = renderTextureColor;
         camera.depthTextureMode = DepthTextureMode.DepthNormals;
         camera.SetTargetBuffers(texture.colorBuffer, texture.depthBuffer);
         camera.Render();
 
-
-
         debugDepthRenderer.sharedMaterial.SetTexture("_MainTex", texture);
-
-        //        print(depthTexture.depthBuffer);
-
-        heightMap = terrainData.heightmapTexture;
-        //        print(terrainData.heightmapTexture);
 
         shader.SetMatrix("_CameraViewMatrix", Camera.main.worldToCameraMatrix);
         shader.SetMatrix("_CameraViewMatrixInverse", Camera.main.worldToCameraMatrix.inverse);
@@ -135,15 +116,12 @@ public class PlaceParticlesOnDepthMap : MonoBehaviour
         shader.SetVector("_ZBufferParams", new Vector4(x, y, z, w));
         shader.SetTexture(kernel, "_DepthTexture", texture);
 
-        shader.SetTexture(kernel, "_HeightMap", terrainData.heightmapTexture);
-        shader.SetVector("_MapSize", terrainData.size);
         shader.SetFloat("_Time", Time.time);
 
         shader.SetBuffer(kernel, "_VertBuffer", _VertBuffer);
         shader.SetInt("_VertBuffer_COUNT", count);
 
         shader.Dispatch(kernel, numGroups, 1, 1);
-
 
         if (mpb == null)
         {
@@ -154,9 +132,6 @@ public class PlaceParticlesOnDepthMap : MonoBehaviour
         mpb.SetInt("_Count", count);
 
         Graphics.DrawProcedural(material, new Bounds(transform.position, Vector3.one * 500000), MeshTopology.Triangles, count * 3 * 2, 1, null, mpb, ShadowCastingMode.Off, true, LayerMask.NameToLayer("Debug"));
-        Graphics.DrawProcedural(material2, new Bounds(transform.position, Vector3.one * 500000), MeshTopology.Triangles, count * 3 * 2, 1, null, mpb, ShadowCastingMode.Off, true, LayerMask.NameToLayer("Debug"));
-        Graphics.DrawProcedural(material3, new Bounds(transform.position, Vector3.one * 500000), MeshTopology.Triangles, count * 3 * 2, 1, null, mpb, ShadowCastingMode.Off, true, LayerMask.NameToLayer("Debug"));
-        // Graphics.DrawProcedural(material4, new Bounds(transform.position, Vector3.one * 500000), MeshTopology.Triangles, count * 3 * 2, 1, null, mpb, ShadowCastingMode.Off, true, LayerMask.NameToLayer("Debug"));
 
     }
 
---
+++ Assets/Plugins/PlaceParticlesOnDepthMap/README.md
@@ -0,0 +1,3 @@
+# README
+
+The particles will render to a layer called 'Debug' . Camera settings wise, this means that your final camera can render everything ( or only debug if you just want to show particles! ) but your 'Depth camera' needs to _NOT_ render the 'Debug layer
