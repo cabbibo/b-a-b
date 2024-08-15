b1dc183cbb8a175deb31f8a2e0ced0615dfccf42
cabbibo <isaaclandoncohen@gmail.com>
2024-08-15 14:21:20 -0700

    milllllller1

--- Assets/Script/WindZone.cs
+++ Assets/Script/WindZone.cs
@@ -1,57 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace WrenUtils{
-public class WindZone : MonoBehaviour
+namespace WrenUtils
 {
-    
-    public float windPower;
-
-    public List<Wren> currentWrens;
-
-    
-    // Start is called before the first frame update
-    void OnEnable()
+    public class WindZone : MonoBehaviour
     {
 
-        currentWrens = new List<Wren>();
-        
-    }
+        public float windPower;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public List<Wren> currentWrens;
 
-        foreach( Wren w in currentWrens ){
 
-            if( !w.state.onGround ){
-                w.physics.rb.AddForce( transform.forward  * windPower);
+        // Start is called before the first frame update
+        void OnEnable()
+        {
+
+            currentWrens = new List<Wren>();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+
+            foreach (Wren w in currentWrens)
+            {
+
+                if (!w.state.onGround)
+                {
+                    w.physics.rb.AddForce(transform.forward * windPower);
+                }
             }
         }
-    }
 
 
-    void OnTriggerEnter( Collider c ){
+        void OnTriggerEnter(Collider c)
+        {
 
-        print(c.tag);
-        if( c.tag == "Wren"){
+            if (God.IsOurWren(c))
+            {
+                currentWrens.Add(c.attachedRigidbody.GetComponent<Wren>());
+            }
 
-            currentWrens.Add( c.attachedRigidbody.GetComponent<Wren>() );
 
         }
-    }
 
-    void OnTriggerExit( Collider c ){
-         print(c.tag);
-        if( c.tag == "Wren"){
+        void OnTriggerExit(Collider c)
+        {
+            if (God.IsOurWren(c))
+            {
 
-            currentWrens.Remove( c.attachedRigidbody.GetComponent<Wren>() );
+                currentWrens.Remove(c.attachedRigidbody.GetComponent<Wren>());
 
+            }
         }
+
+
+
     }
-
-
-
-}
 }
