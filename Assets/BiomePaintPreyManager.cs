230d8f33d8c7d178497ffb6a528e2320b0f6ecc4
mklitsner <mklitsner@gmail.com>
2024-09-05 13:23:17 -0700

    Made "Chase Me" script and set up in Miller Island
    # Conflicts:
    #	Assets/MillerStuff.meta
    #	Assets/MillerStuff/ChaseMe Holder.prefab.meta
    #	Assets/Samples/Splines/2.5.2/Spline Examples (requires Shader Graph package)/Scenes/GPU Evaluation/GPU Evaluation.unity.meta
    #	Assets/Samples/Splines/2.5.2/Spline Examples (requires Shader Graph package)/Scenes/Instantiate Component/SplineInstantiate.unity.meta
    #	Assets/tracePackage2.unitypackage.meta
    #	Assets/tracePackage3.unitypackage.meta

--- Assets/BiomePaintPreyManager.cs
+++ Assets/BiomePaintPreyManager.cs
@@ -10,8 +10,6 @@ public class BiomePaintPreyManager : PreyManager
 
     public int foodType;
 
-    public float spawnDistance;
-
 
     public override void CheckForNewPrey()
     {
@@ -27,43 +25,6 @@ public class BiomePaintPreyManager : PreyManager
                 SpawnNewBug();
             }
         }
-
-    }
-
-    public override void SpawnNewBug()
-    {
-
-        // destroy any over max
-        while (preyHolder.childCount >= maxPray)
-        {
-            DestroyImmediate(preyHolder.GetChild(0).gameObject);
-        }
-
-
-        Vector3 spawnPos = God.wren.transform.position + God.wren.transform.forward * spawnDistance;
-
-        spawnPos.y = 10000;
-
-        RaycastHit hit;
-        if (Physics.Raycast(spawnPos, Vector3.down, out hit, 20000))
-        {
-            spawnPos = hit.point + Vector3.up * spawnRadius * 2;
-        }
-
-
-        Vector3 offset = Random.insideUnitSphere * spawnRadius;
-
-        spawnPos += offset;
-
-        PreyController newPrey = Instantiate(preyPrefab, spawnPos, Quaternion.identity).GetComponent<PreyController>();
-
-        newPrey.Initialize(preyConfig, this);
-
-
-        newPrey.transform.parent = preyHolder;
-
-
-        lastSpawnTime = Time.time;
 
     }
 
